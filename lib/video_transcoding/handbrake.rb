81431020cea3c13ba0871f19e0dc3eb5aa34a6c5
Don Melton <don@blivet.com>
2018-06-18 17:20:05 -0700

    Now require `HandBrakeCLI` version 1.0.0 or later.

--- lib/video_transcoding/handbrake.rb
+++ lib/video_transcoding/handbrake.rb
@@ -11,69 +11,20 @@ module VideoTranscoding
     COMMAND_NAME = 'HandBrakeCLI'
 
     def setup
-      Tool.provide(COMMAND_NAME, ['--preset-list']) do |output, status, _|
-        fail "#{COMMAND_NAME} failed during execution" unless status == 0
+      Tool.provide(COMMAND_NAME, ['--version']) do |output, status, _|
+        fail "#{COMMAND_NAME} version 1.0.0 or later required" unless status == 0
 
-        if output =~ /^HandBrake ([^h][^ ]+)/
-          version = $1
-          Console.info "#{$MATCH} found..."
-
-          unless  (version =~ /^([0-9]+)\.([0-9]+)/ and (($1.to_i * 100) + $2.to_i) >= 10) or
-                  (version =~ /^svn([0-9]+)/ and $1.to_i >= 6536)
-            fail "#{COMMAND_NAME} version 0.10.0 or later required"
-          end
+        unless output =~ /^HandBrake ([^h][^ ]+)/
+          Console.debug output
+          fail "#{COMMAND_NAME} version unknown"
         end
-      end
 
-      begin
-        IO.popen([Tool.use(COMMAND_NAME), '--version'], :err=>[:child, :out]) do |io|
-          if io.read =~ /^HandBrake ([^h][^ ]+)/
-            Console.info "#{$MATCH.strip} found..."
-          end
-        end
-      rescue SystemCallError => e
-        raise "#{COMMAND_NAME} failed during execution: #{e}"
+        Console.info "#{$MATCH.strip} found..."
       end
     end
 
     def command_name
       Tool.use(COMMAND_NAME)
-    end
-
-    def auto_anamorphic
-      properties = Tool.properties(COMMAND_NAME)
-
-      unless properties.has_key? :auto_anamorphic
-        if help_text =~ /auto-anamorphic/
-          properties[:auto_anamorphic] = 'auto-anamorphic'
-        else
-          properties[:auto_anamorphic] = 'strict-anamorphic'
-        end
-      end
-
-      properties[:auto_anamorphic]
-    end
-
-    private
-
-    def help_text
-      properties = Tool.properties(COMMAND_NAME)
-
-      unless properties.has_key? :help_text
-        properties[:help_text] = ''
-
-        begin
-          IO.popen([Tool.use(COMMAND_NAME), '--help'], :err=>[:child, :out]) do |io|
-            properties[:help_text] = io.read
-          end
-        rescue SystemCallError => e
-          raise "#{COMMAND_NAME} help text unavailable: #{e}"
-        end
-
-        fail "#{COMMAND_NAME} failed during execution" unless $CHILD_STATUS.exitstatus == 0
-      end
-
-      properties[:help_text]
     end
   end
 end
