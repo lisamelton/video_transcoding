3a026833ac36fce5438dffdd9b85d1f4686cca8e
Lisa Melton <115488+lisamelton@users.noreply.github.com>
2025-01-09 09:58:22 -0800

    Deprecate and remove legacy project files.

--- lib/video_transcoding/copyright.rb
+++
@@ -1,9 +0,0 @@
-#
-# copyright.rb
-#
-# Copyright (c) 2025 Lisa Melton
-#
-
-module VideoTranscoding
-  COPYRIGHT = 'Copyright (c) 2025 Lisa Melton'
-end
--- lib/video_transcoding/version.rb
+++
@@ -1,9 +0,0 @@
-#
-# version.rb
-#
-# Copyright (c) 2025 Lisa Melton
-#
-
-module VideoTranscoding
-  VERSION = '0.25.4'
-end
